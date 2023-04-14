e7328d41748e8186fdae4fbb1e896fca7e527253
HugoSanche <hbmclaren@gmail.com>
2023-04-13 23:40:08 -0600

    Working with HashMap

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -10,7 +11,31 @@ public class Main {
         list.add(new Laptop("ASUS", 16, 25000));
         list.add(new Laptop("Lenovo", 16, 19500));
         list.add(new Laptop("APPLE", 16, 35600));
+        
+        System.out.println("Using Comparable");
         Collections.sort(list);
-        System.out.println("List of laptops "+list);
+        for (Laptop l: list){
+            System.out.println(l);
+        }
+
+        Comparator<Laptop> com =new Comparator<Laptop>() {
+
+            @Override
+            public int compare(Laptop l1, Laptop l2) {
+              if (l1.getPrice()>l2.getPrice())
+                return 1;
+                else if (l1.getPrice()==l2.getPrice())
+                    return 0;
+                    else {
+                        return -1;
+                    }
+            }
+            
+        };
+        System.out.println("Using Comparator");
+        Collections.sort(list,com);
+        for (Laptop l2:list){
+            System.out.println(l2);
+        }
      }
 }
